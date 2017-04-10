5961b0068be82c456b3a24f2c65894586153ae96
V Jay <codesmith111@gmail.com>
2017-04-10 11:15:25 -0400

    updated test1

--- src/test1.js
+++ src/test1.js
@@ -13,13 +13,13 @@ const promiseWithErrors = (id, message, makeError) =>{
 
 //Created this to make test the then call. function takes one parameter that it does not use
 const promiseWithErrors1 = (val) =>{
-    console.log(val)
+    console.log("Message from previous promise--" +val)
     return new Promise(
         (resolve, reject)=>reject('OOOPS - Error in Promise')
     )
 }
 
 let promise = promiseWithErrors(1, "Creating promise with errors", false)
-            .then(()=>promiseWithErrors1())  //no problems now because the function accepts on parameter      
+            .then(promiseWithErrors1)  //no problems now because the function accepts on parameter    
             .catch(e => console.log(e))  
- 
+
